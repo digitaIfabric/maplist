ede7c7161d348716248e250398604e5818aa48d2
mcemolcusenler <cem.olcusenler@gmail.com>
2017-10-26 00:15:34 +0000

    first commit on the branch

---
+++ public/scripts/sidebar.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#likes-display").click(function(){
+    $("#likes-div").animate({left: '70px'});
+  });
+  $("#contributor-display").click(function(){
+    $("#contributor-div").animate({left: '70px'});
+  });
+});
